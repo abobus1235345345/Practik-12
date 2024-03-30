499e6602629060426cfba079adc0a1e7b90a6595
abobus1235345345 <139453779+abobus1235345345@users.noreply.github.com>
2024-03-30 10:09:38 +0300

    Add files via upload

---
+++ PRACTIC12/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Введите ваше имя:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите вашу фамилию:");
+        string surname = Console.ReadLine();
+
+        string[] lines = await ReadFileAsync("sample.txt");
+
+        Console.WriteLine($"Привет, {name} {surname}!");
+
+        Console.WriteLine("Хотите увидеть строки из файла? (Y/N)");
+        string choice = Console.ReadLine();
+
+        if (choice.ToUpper() == "Y")
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+
+    static async Task<string[]> ReadFileAsync(string fileName)
+    {
+        string[] lines = await File.ReadAllLinesAsync(fileName);
+        return lines;
+    }
+}
